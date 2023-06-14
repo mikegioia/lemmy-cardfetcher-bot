15476c86d5ed327c5c8a26a2dd2391d066cc6690
Mike Gioia <mikegioia@gmail.com>
2023-06-13 20:34:19 -0400

    Updated bot to return links when searching cards. Added eslint and prettier.

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,17 @@
 import LemmyBot, { CommentView, PostView } from 'lemmy-bot';
 import { config } from 'dotenv';
-import { ScryfallCardObject } from "./scryfall";
+import { ScryfallCardObject } from './scryfall';
+import distance = require('jaro-winkler');
 
 config();
 
-const { INSTANCE, USERNAME_OR_EMAIL, PASSWORD, SCRYFALL_API_KEY } =
-  process.env as Record<string, string>;
+const { INSTANCE, USERNAME_OR_EMAIL, PASSWORD } = process.env as Record<
+  string,
+  string
+>;
 
-const scryfallEndpoint = "https://api.scryfall.com/cards/search?q=";
 const gathererRegex = new RegExp(/(?<=\[\[)(.*?)(?=\]\])/g);
+const scryfallEndpoint = 'https://api.scryfall.com/cards/search?q=';
 
 /**
  * Reads in a post or comment message and pulls out any
@@ -18,59 +21,111 @@ const gathererRegex = new RegExp(/(?<=\[\[)(.*?)(?=\]\])/g);
  *
  * @param {string} message
  */
-function searchCards (message: string): void {
+function searchCards(message: string): void {
   const gathererCards = message.match(gathererRegex);
+  const cards: string[] = [];
 
-  if (gathererCards) {
-    gathererCards.forEach((card: string) => {
-      fetchAndReturn(card)
-    });
+  if (!gathererCards) {
+    return;
   }
+
+  gathererCards.forEach((card: string) => {
+    fetchAndGetResponseLine(card);
+  });
+
+  console.log(cards);
 }
 
 /**
- * Searches the Scryfall database for a match on the card name
- * and then returns a link to the card if found.
+ * Searches the Scryfall database for a match on the
+ * card name and then returns a string containing links
+ * to the card if found.
  *
  * @param {string} card
  */
-function fetchAndReturn (card: string): void {
+function fetchAndGetResponseLine(card: string): void {
   const encoded = encodeURI(card);
 
-  console.log('fetching', card);
-
-  fetch(scryfallEndpoint+encoded)
+  fetch(scryfallEndpoint + encoded)
     .then((response: any) => response.json())
     .then((scryfallResponse: any) => {
       const cardList = scryfallResponse.data;
 
-      console.log('response', cardList);
-
-      if (cardList === null) {
-        console.log(`Unable to retrieve information for "${card}"`)
-      } else {
-        //sendGathererInfo(pickBest(card, cardList));
-        console.log('sendGathererInfo');
+      if (!cardList) {
+        console.log(`Unable to retrieve information for "${card}"`);
       }
+
+      getResponseLine(pickBestCard(card, cardList));
     });
+}
+
+/**
+ * Looks for the best match for the card name in the set
+ * of cards found in the scryfall search.
+ *
+ * @param {string}               cardName
+ * @param {ScryfallCardObject[]} cardList
+ */
+function pickBestCard(
+  cardName: string,
+  cardList: ScryfallCardObject[]
+): ScryfallCardObject {
+  let index = 0;
+  let max = Number.NEGATIVE_INFINITY;
+
+  cardList.forEach((card, i) => {
+    const num = distance(card.name.toLowerCase(), cardName.toLowerCase());
+
+    if (num > max) {
+      max = num;
+      index = i;
+    }
+  });
+
+  return cardList[index];
+}
+
+/**
+ * Returns a single line in the comment reply text
+ * containing all of the different links to the card:
+ *
+ *   `Card Name - (G) (SF) (txt)`
+ *
+ * Card Name: Scryfall image link
+ *       (G): Gatherer web link
+ *      (SF): Scryfall web link
+ *     (txt): Scryfall text link
+ *
+ * @param {ScryfallCardObject} card
+ */
+function getResponseLine(card: ScryfallCardObject): void {
+  const utmSource = 'utm_source=lemmy';
+
+  const responseLine =
+    `[${card.name}](${card.image_uris.normal}&${utmSource}) - ` +
+    `[(G)](${card.related_uris.gatherer}&${utmSource}) ` +
+    `[(SF)](${card.scryfall_uri}) ` +
+    `[(txt)](${card.uri}?${utmSource}&format=text)`;
+
+  console.log(responseLine + '\n');
 }
 
 const bot = new LemmyBot({
   instance: INSTANCE,
   credentials: {
     username: USERNAME_OR_EMAIL,
-    password: PASSWORD,
+    password: PASSWORD
   },
   connection: {
-    secondsBetweenPolls: 30,
+    secondsBetweenPolls: 30
   },
   federation: {
     allowList: [
       {
         instance: INSTANCE,
         communities: ['sandbox']
-      },
-    ],
+      }
+    ]
   },
   dbFile: 'db.sqlite3',
   handlers: {
@@ -81,7 +136,7 @@ const bot = new LemmyBot({
         },
         botActions: { createComment }
       }) => {
-        console.log(id+': '+content);
+        console.log(id + ': ' + content);
       }
     },
     post: {
@@ -91,10 +146,13 @@ const bot = new LemmyBot({
         },
         botActions: { createComment }
       }) => {
-        console.log('post: ['+id+']');
+        console.log('post: [' + id + ']');
       }
     }
   }
 });
 
-bot.start();
+// bot.start();
+searchCards(
+  "My favorite cards are [[Verdant Force]] and [[Stitcher's Supplier]], what are yours?"
+);
